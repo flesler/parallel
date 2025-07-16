74cb1a46e8123c59cdc76a6818d64502545db6df
Ariel Flesler <aflesler@gmail.com>
2025-07-16 14:15:04 -0300

    Improve the readme.js code

--- bin/readme.js
+++ bin/readme.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
-
 const fs = require('fs')
 const help = require('../lib/help')
 const util = require('../lib/util')
 
 // Get the help output directly from the help module
-const helpOutput = help.getText()
+const output = help.getText()
 
-let markdown = helpOutput
+const markdown = output
   .replace(/^(.+):\n/gm, '# $1\n\n```bash\n')
   .replace(/^( {2}.*\n)\n/gm, '$1```\n\n')
   .replace(/^ {2}/gm, '')
-
-const lines = markdown.split('\n').slice(2, -2)
-const firstLine = lines[0].trim()
-const lastLine = '# Input from command-line arguments'
+  .split('\n').slice(2, -2).join('\n').trim()
 
 const readmePath = 'README.md'
 const readme = fs.readFileSync(readmePath, 'utf8')
 
-const pattern = new RegExp(`${util.escapeRegex(firstLine)}[\\s\\S]*?(?=\n+${util.escapeRegex(lastLine)})`)
-markdown = lines.join('\n').trim()
+const firstLine = util.escapeRegex(markdown.split('\n')[0])
+const lastLine = util.escapeRegex('# Input from command-line arguments')
+
+const pattern = new RegExp(`${firstLine}[\\s\\S]*?(?=\n+${lastLine})`)
 const updatedReadme = readme.replace(pattern, () => markdown)
 
 if (updatedReadme === readme) {
