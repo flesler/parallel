fff7d5aecd8cd00fd991ff0abdd411a3988a1b6a
Ariel Flesler <aflesler@gmail.com>
2025-07-16 00:04:07 -0300

    Add a new changelog.js script

---
+++ bin/changelog.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const { readFileSync, writeFileSync } = require('fs')
+const { execSync } = require('child_process')
+
+// $ node bin/changelog.js
+
+function exec(...args) {
+  const command = args.join(' ')
+  console.log('$', command)
+  return execSync(command)
+}
+
+function skipSomeLines() {
+  const lines = readFileSync('CHANGELOG.md', 'utf8').split('\n').filter(line => (
+    !/^> /.test(line)
+  ))
+  const out = lines.join('\n').replace(/\n{3,}/g, '\n\n')
+  writeFileSync('CHANGELOG.md', out)
+}
+
+exec('./node_modules/.bin/auto-changelog', '--commit-limit', '10000', '--backfill-limit', '10000', '--sort-commits', 'date', '--hide-credit')
+skipSomeLines()
